34699150c20da9701cf216ddf77b51a373ba13a9
denrahydnas <sandylin34@gmail.com>
2021-06-27 13:22:16 -0400

    removing body-parser

--- public/js/sofetch.js
+++ public/js/sofetch.js
@@ -75,7 +75,7 @@ async function postData(url, book) {
     });
     console.log(JSON.stringify(book));
     return response.json(); // parses JSON response into native JavaScript objects
-  }
+};
 
 
 
